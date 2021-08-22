32d2904af0541871640de10d3e1aec8904a2edfe
John McGorey <j.t.mcgorey@gmail.com>
2021-08-22 00:53:59 -0500

    Added Simple UI & Reordered main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,34 @@
 import {getRandomValueFromList} from "./random.js";
 import {titanfall2Options} from "./titanfall-two-values.js";
 
-const randomValues = titanfall2Options.map(function(optionSet) {
-    return {
-        category: optionSet.singularLabel,
-        value: getRandomValueFromList(optionSet.values).name
-    };
-});
 
-console.log('Random Values: ');
-randomValues.forEach(val => console.log(val));
+const valueMap = {};
+
+function randomizeAll() {
+    const randomValues = titanfall2Options.map(function(optionSet) {
+        return {
+            categoryLabel: optionSet.singularLabel,
+            categoryName: optionSet.name,
+            value: getRandomValueFromList(optionSet.values).name
+        };
+    });
+
+    for(const val of randomValues) {
+        valueMap[val.categoryName] = val.value;
+    }
+}
+
+function updateUi() {
+    document.getElementById('tactical').innerText = valueMap[ 'tacticals'];
+    document.getElementById('ordinance').innerText = valueMap[ 'ordnances'];
+    document.getElementById('primaryWeapon').innerText = valueMap[ 'primaryWeapons'];
+    document.getElementById('antiTitanWeapon').innerText = valueMap[ 'antiTitanWeapons'];
+    document.getElementById('pistol').innerText = valueMap[ 'pistols'];
+    document.getElementById('kit1').innerText = valueMap[ 'kit1'];
+    document.getElementById('kit2').innerText = valueMap[ 'kit2'];
+    document.getElementById('boost').innerText = valueMap[ 'boosts'];
+    document.getElementById('titan').innerText = valueMap[ 'titans'];
+}
+
+randomizeAll();
+updateUi();
